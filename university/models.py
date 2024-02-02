18bbbd386815dfdd0a316882c51a8f552084a5db
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2024-02-02 09:59:33 +0100

    Fix about button

--- university/models.py
+++ university/models.py
@@ -15,4 +15,13 @@ class Student(models.Model):
 
     def __str__(self):
         return f"Student: {self.fName} {self.lName}"
+    
+class FieldOfStudy(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
 # Add finished school document field
